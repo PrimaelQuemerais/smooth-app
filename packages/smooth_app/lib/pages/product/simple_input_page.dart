bea66f41e76fc64068b0c02fef96766bc34d8b53
Primaël Quémerais <34269530+PrimaelQuemerais@users.noreply.github.com>
2025-02-18 17:50:40 +0100

    feat: adds robotoff questions to SimpleInputPage

--- packages/smooth_app/lib/pages/product/simple_input_page.dart
+++ packages/smooth_app/lib/pages/product/simple_input_page.dart
@@ -65,7 +65,8 @@ class _SimpleInputPageState extends State<SimpleInputPage> {
     final List<String> titles = <String>[];
 
     for (int i = 0; i < widget.helpers.length; i++) {
-      titles.add(widget.helpers[i].getTitle(appLocalizations));
+      final AbstractSimpleInputPageHelper helper = widget.helpers[i];
+      titles.add(helper.getTitle(appLocalizations));
       simpleInputs.add(
         Padding(
           padding: i == 0
@@ -81,11 +82,11 @@ class _SimpleInputPageState extends State<SimpleInputPage> {
                 },
               ),
               ChangeNotifierProvider<AbstractSimpleInputPageHelper>(
-                create: (_) => widget.helpers[i],
+                create: (_) => helper,
               ),
             ],
             child: SimpleInputWidget(
-              helper: widget.helpers[i],
+              helper: helper,
               product: widget.product,
               controller: _controllers[i],
               displayTitle: true,
